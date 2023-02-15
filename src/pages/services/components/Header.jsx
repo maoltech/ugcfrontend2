afaa6f6a34cdb0a13ec30a9a782d19d5cf81b48f
maoltech <hardeyshinnah.mo@gmail.com>
2023-02-15 12:59:18 -0500

    service side

--- src/pages/services/components/Header.jsx
+++ src/pages/services/components/Header.jsx
@@ -1,5 +1,9 @@
 import Logo from "../../../assets/logo.svg";
 import Button from "../../../global/Button";
+import { TbNews } from "react-icons/tb";
+import { HiOutlineBell } from "react-icons/hi";
+import {MdKeyboardArrowDown} from "react-icons/md"
+
 const Header = () => {
   return (
     <div className="w-full bg-[#F8FAFC] shadow-[0px 2px 6px] shadow-[#0000]/[6%]">
@@ -10,16 +14,22 @@ const Header = () => {
             alt="logo"
             className="w-[100px] h-[60px] justify-self-start"
           />
-          <Button className={"bg-[#0030AB] text-white"}>Post Content</Button>
-          <Button>Browse Services</Button>
-          <Button>How it Works</Button>
         </div>
         <div className="flex items-center justify-end">
-          <Button>Sign Up</Button>
-          <Button>Log In</Button>
-          <Button className={"bg-[#0030AB]/[6%] text-[#0030AB]"}>
-            Become a seller
-          </Button>
+          <Button>Services</Button>
+          <Button>Orders</Button>
+          <TbNews className={"text-[#9CA3AF] w-[45px] h-[26.44444465637207px] border-0 rounded-none"}/>
+          <HiOutlineBell className={"text-[#9CA3AF] w-[45px] h-[26.44444465637207px] border-0 rounded-none"}/>
+          <div className={"bg-[#0030AB]/[6%] w-[46px] h-[46px] rounded-full text-[#111827]"}>
+            <div>            
+              Robert Michaelis
+            </div>
+
+            <MdKeyboardArrowDown />
+          </div>
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
